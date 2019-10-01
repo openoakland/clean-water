43ee88b67fb7e62f2c1af686f6495747ffa693d2
Alex Aruj <aaruj@aaruj-ltm.internal.salesforce.com>
2019-10-01 13:14:54 -0700

    collapsible table using button - test

--- docs/index.js
+++ docs/index.js
@@ -79,3 +79,18 @@ function resetElements(currentName, currentIndex) {
   document.querySelector('select[name="assembly"]').selectedIndex = 0;
   document.querySelector('select[name="'+currentName+'"]').selectedIndex = currentIndex;
 }
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
