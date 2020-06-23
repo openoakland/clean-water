64f3007d5ee0ec3e5155a24cb26e0bbf0ce11c8f
Mei Eisenbach <mei.eisenbach@gmail.com>
2020-06-22 17:25:42 -0700

    Refactored system.html and related files

--- docs/system.js
+++ docs/system.js
@@ -8,47 +8,33 @@ fetch('data/'+waterSystemId+'.json')
   return response.json();
 })
 .then(function(systemData) {
-
-  let uniqueSystemData = [];
-  systemData.forEach( (item) => {
-    let matchFound = false;
-    uniqueSystemData.forEach( (existingItem) => {
-      if(item.ANALYTE_NAME == existingItem.ANALYTE_NAME && item.VIOL_BEGIN_DATE == existingItem.VIOL_BEGIN_DATE && item.VIOL_END_DATE == existingItem.VIOL_END_DATE) {
-        // skip
-        // matchFound = true;
-      }
-    })
-    if(!matchFound) {
-      uniqueSystemData.push(item);
-    }
-  })
-
   // split these by unique analyte
   let uniqueAnalyteMap = new Map();
-  uniqueSystemData.forEach( (item) => {
+  systemData.forEach( (item) => {
     let foundAnalyte = uniqueAnalyteMap.get(item.ANALYTE_NAME)
     if(typeof(foundAnalyte) == 'undefined') {
       uniqueAnalyteMap.set(item.ANALYTE_NAME,[item]);
     } else {
       updateList(uniqueAnalyteMap,item.ANALYTE_NAME,item);
     }
-  })
+  });
+
+  document.querySelector('h1').innerHTML = "Water System: " + systemData[0].WATER_SYSTEM_NAME;
+  document.querySelector('#population').innerHTML = parseInt(systemData[0].POPULATION).toLocaleString();
+  document.querySelector('#city').innerHTML = "City of " + systemData[0].CITY;
+  document.querySelector('#zipcode').innerHTML = systemData[0].ZIPCODE;
+  document.querySelector('#county').innerHTML = systemData[0].COUNTY + " County";
+  document.querySelector('#reg_agency').innerHTML = "Regulating Agency: "+ systemData[0].REGULATING_AGENCY;
+  
+  let analytes = [];
 
   let output = `
-  <h1>Water System: ${systemData[0].WATER_SYSTEM_NAME}</h1>
-  <h2 style="text-align: center; font-weight: 500;">${parseInt(systemData[0].POPULATION).toLocaleString()} People affected</h2>
-  <h3 class="erf-align">${systemData[0].CITY}, ${systemData[0].ZIPCODE}</h3>
-  <h3 class="erf-align">${systemData[0].COUNTY} COUNTY</h3>
-  <h3 class="erf-align">Regulating Agency:  ${systemData[0].REGULATING_AGENCY}</h3>
-  <br><br>
-  <h3 class="erf-align">The following table(s) show all measurements for all analytes which exceeded allowed levels</h3>
-  <br><br>
-  <div align="left">
-  <button id="export-list">Export Displayed Data</button>
-  </div>
   ${Array.from(uniqueAnalyteMap).map((analyte) => {
+    if (analyte[1].length > 0 && (analyte[1][analyte[1].length - 1].ENF_ACTION_TYPE_ISSUED) == "RETURN TO COMPLIANCE")
+      return '';
+    analytes.push(analyte[0]);
     return `
-      <h2 class="erf-align">${analyte[0]} - ${analyte[1][0].VIOLATION_TYPE_NAME}</h2>
+      <h2>${analyte[0]} - ${analyte[1][0].VIOLATION_TYPE_NAME}</h2>
       <table class="violaters system-specific">
       <tr>
         <th class="head">Violation Begin Date</th>
@@ -72,7 +58,7 @@ fetch('data/'+waterSystemId+'.json')
         <th class="head" style="display:none">Enforcement Action Issue Date</th>
         </tr>
         <!-- need to loop through all violations -->
-        ${uniqueSystemData.map((item) => {
+        ${systemData.map((item) => {
           if(item.ANALYTE_NAME == analyte[0]) {
             var absexc = item.RESULT.split(' ')[0] - item.MCL.split(' ')[0];
             var pctexc = absexc/item.MCL.split(' ')[0]*100;
@@ -105,13 +91,10 @@ fetch('data/'+waterSystemId+'.json')
   }).join(' ')}
   `;
 
-  document.querySelector('.system-history').innerHTML = output;
-
-    document.getElementById("export-list").addEventListener("click", function() {
-      exportList(document.querySelectorAll('table tr'));
-
+  document.getElementById("export-list").addEventListener("click", function() {
+    exportList(document.querySelectorAll('table tr'));
   })
+
+  document.querySelector('#analytes').innerHTML = analytes.join(', ');
+  document.querySelector('#tables').innerHTML = output;
 })
-/*
-{"REGULATING_AGENCY":"DISTRICT 19 - TEHACHAPI","WATER_SYSTEM_NUMBER":"CA1500459","WATER_SYSTEM_NAME":"LAKE OF THE WOODS MOBILE VILLAGE","CLASSIFICATION":"COMMUNITY","POPULATION":"82","SERVICE_CONNECTIONS":"86","COUNTY":"KERN","CITY":"LAKE OF THE WOODS","ZIPCODE":"93225","VIOLATION_NUMBER":9619006,"VIOLATION_TYPE_NAME":"MCL, SINGLE SAMPLE","ANALYTE_NAME":"NITRATE","RESULT":"12 MG/L","MCL":"10 MG/L","VIOL_BEGIN_DATE":"2016-04-01T07:00:00.000Z","VIOL_END_DATE":"2016-06-30T07:00:00.000Z","ENF_ACTION_NUMBER":719011,"ENF_ACTION_ISSUE_DATE":"2019-06-26T07:00:00.000Z","ENF_ACTION_TYPE_ISSUED":"STATE INTENTIONAL NO ACTION TAKEN"}
-*/
